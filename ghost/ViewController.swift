06179d36aa3236436d73c3418a35cd07ced472b9
william moraes <william.moraes57@gmail.com>
2021-09-03 23:13:24 -0300

    Initial Commit

---
+++ ghost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ghost
+//
+//  Created by William Moraes da Silva on 03/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
